0398b07f0e35f7136e6adf282b375b7cf565c02b
Tilak <tilaksharma1114@gmail.com>
2024-06-07 11:53:56 +0000

    Add mbodi headers

--- src/mbodied_agents/agents/motor/rt1/film_efficientnet/film.py
+++ src/mbodied_agents/agents/motor/rt1/film_efficientnet/film.py
@@ -1,10 +1,10 @@
-# Copyright 2024 Simply Robots Inc.
+# Copyright 2024 Mbodi AI
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -66,7 +66,7 @@ def film_conditioned(cls):
 
 
 class FilmLayer(nn.Module):
-    '''Layer to conditionally modulate the input tensor with the context tensor.'''
+    """Layer to conditionally modulate the input tensor with the context tensor."""
 
     def __init__(
         self,
