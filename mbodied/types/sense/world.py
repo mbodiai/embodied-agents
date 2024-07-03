7014c9ebbb0d7e41be50b6d1e2be2d8fe2a37e8f
Zaid-Waghoo <zw3374@nyu.edu>
2024-07-03 18:23:26 -0400

    Fixes and updates

--- mbodied/types/sense/world.py
+++ mbodied/types/sense/world.py
@@ -5,19 +5,15 @@ from mbodied.base.sample import Sample
 from mbodied.types.sense.vision import Image
 from functools import partial
 from mbodied.types.geometry import Pose6D
-    
-class SceneObjects(Sample):
-    """Model for Scene Object."""
-    object_name: List[str] | str = Field(default_factory=str, description="Name of the object in the scene")
 
-class ObjectsPose(Sample):
-    """Model for Object Pose."""
-    object_pose: List[Pose6D] | Pose6D = Field(default_factory=Pose6D, description="Pose of the object with respect to a world frame")
+class SceneObject(Sample):
+    """Model for Scene Object Poses."""
+    object_name: str = Field(default=lambda: str, description="The name of an object in the scene")
+    object_pose: Pose6D = Field(default_factory=lambda: [Pose6D()], description="Pose of the object with respect to a reference frame")
 
 
 class SceneData(Sample):
     """Model for Scene Data."""
-    image: Image = Field(default_factory=partial(Image, size=(224,224)), description="Image of the scene")
-    objects: SceneObjects = Field(default_factory=lambda: [SceneObjects()], description="List of objects in the scene")
-    object_poses: ObjectsPose = Field(default_factory=lambda: [Pose6D()], description="List of object poses in the scene")
+    image: Image = Field(default_factory=partial(Image, size=(224,224)), description="Annotated Image")
+    scene_objects: List[SceneObject] = Field(default_factory=lambda: [SceneObject()], description="List of Scene Objects")
 
